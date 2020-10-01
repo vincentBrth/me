ae5f8bdac15ee6f9c55bae1dd65c824c456d80b7
Vincent Berthet <vincent.berthet42@gmail.com>
2020-10-01 21:52:35 +0200

    Develop (#9)
    * add bcom integration report
    
    * change content description for request
    
    * fix jpg
    
    * use planet hoster to host monitoring and uws blog
    
    * change  color of icon project link
    
    * add youtube link to Luna and helios gameplay
    
    * update readme
    
    * change scogue link to github source
    
    * change workspace repository name
    
    * update bcom project
    
    * darker project icons
    
    * add shiftmode navigation
    
    * v0 shiftmode
    
    * v1 shiftmode, rework request pages
    
    * request ok
    
    * clean code 1
    
    * clean code 2
    
    * clean 3

--- js/effects.js
+++ js/effects.js
@@ -2,18 +2,12 @@
     'use strict';
 
     jQuery(document).ready(function () {
-
-        
-       /* Preloader */
-		
+        /* Preloader */
         $(window).on('load', function() {
-          $('body').addClass('loaded');
+            $('body').addClass('loaded');
         });
 		
-		
-		
-       /* Smooth Scroll */
-
+        /* Smooth Scroll */
         $('a.smoth-scroll').on("click", function (e) {
             var anchor = $(this);
             $('html, body').stop().animate({
@@ -21,12 +15,8 @@
             }, 1000);
             e.preventDefault();
         });
-		
 
-
-       
-       /* Scroll Naviagation Background Change with Sticky Navigation */
-		 
+        /* Scroll Naviagation Background Change with Sticky Navigation */          
         $(window).on('scroll', function () {
             if ($(window).scrollTop() > 100) {
                 $('.header-top-area').addClass('navigation-background');
@@ -34,12 +24,8 @@
                 $('.header-top-area').removeClass('navigation-background');
             }
         });
-		
-		
-		
-		
-       /* Mobile Navigation Hide or Collapse on Click */
-		
+
+        /* Mobile Navigation Hide or Collapse on Click */
         $(document).on('click', '.navbar-collapse.in', function (e) {
             /*if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
                 $(this).collapse('hide');
@@ -49,25 +35,20 @@
         $('body').scrollspy({
             target: '.navbar-collapse',
             offset: 195
-		 });
-		 
-
+            });
+            
         /* Scroll To Top */
-		
         $(window).scroll(function(){
         if ($(this).scrollTop() >= 500) {
             $('.scroll-to-top').fadeIn();
-         } else {
+            } else {
             $('.scroll-to-top').fadeOut();
-         }
-	   });
-	
-	
-	    $('.scroll-to-top').click(function(){
-		  $('html, body').animate({scrollTop : 0},800);
-		  return false;
-	    });
+            }
+        });
 
+        $('.scroll-to-top').click(function(){
+            $('html, body').animate({scrollTop : 0},800);
+            return false;
+        });
     });
-
-   })(jQuery);
+})(jQuery);
